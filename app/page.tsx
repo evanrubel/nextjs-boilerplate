f570900dca49669e6701d7d3b49ca7b9863df443
Evan Rubel <erubel@mit.edu>
2023-12-26 23:17:38 -0800

    add time of day utils

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import Footer from "./components/Footer";
-import Panel from "./components/Panel";
+// import Panel from "./components/Panel";
 import Profile from "./components/Profile";
+import { getTimeOfDayString } from "./utils/utils";
 
 export default function Home() {
-  const panels = ["About", "Projects", "Contact"];
+  // const panels = ["About", "Projects", "Contact"];
+  {
+    /* {panels.map((panel) => (
+        <Panel key={panel} />
+      ))} */
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Profile />
-      {panels.map((panel) => (
-        <Panel key={panel} />
-      ))}
+
+      <h3 className="mt-4 font-italics">
+        Good {getTimeOfDayString()}! My name is Evan, and I&#39;m a fourth-year
+        student at MIT studying Computer Science.
+      </h3>
       <Footer />
     </main>
   );
---
+++ app/utils/utils.ts
@@ -0,0 +1,11 @@
+export const getTimeOfDayString = (): string => {
+  const currentHour = new Date().getHours();
+
+  if (currentHour < 12) {
+    return "morning";
+  } else if (currentHour < 18) {
+    return "afternoon";
+  } else {
+    return "evening";
+  }
+};
