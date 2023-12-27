dcf768930e5a6b2714fb0ed2dce4aa3179204391
Evan Rubel <erubel@mit.edu>
2023-12-26 23:05:04 -0800

    Updated socials

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import Footer from "./components/Footer";
+import Panel from "./components/Panel";
+import Profile from "./components/Profile";
 
 export default function Home() {
+  const panels = ["About", "Projects", "Contact"];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Profile />
+      {panels.map((panel) => (
+        <Panel key={panel} />
+      ))}
+      <Footer />
+    </main>
   );
 }
